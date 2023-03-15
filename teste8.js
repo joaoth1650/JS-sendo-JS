65cf703686082a4800acc104ffa6b2a73fe7adc2
joaoth <joaopedromcj9@gmail.com>
2023-03-15 18:05:14 -0300

    okvamosla

--- teste8.js
+++ teste8.js
@@ -1,7 +1,11 @@
 function miniMaxSum(arr) {
   // Write your code here
-     
+      
+   let sominha = 0;
+   let somona = 0;
+
+   for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
     
-
-
+   }
 }
