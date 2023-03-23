3258fe7c68f6072622c0ced4cd32ad4f06727c8c
joaoth <joaopedromcj9@gmail.com>
2023-03-23 17:03:41 -0300

    AulasSenhoresAulas

---
+++ aprendendoJS/maiuscula e minuscula.js
@@ -0,0 +1,5 @@
+//transforme letras minusculas em maiusculas. Faça o contrario disso tambem
+
+let word = "programar é muito bacana!"
+
+console.log(word.toLowerCase().toUpperCase())
---
+++ aprendendoJS/manipulando string.js
@@ -0,0 +1,4 @@
+// tranformando um numero qbrado com 2 casas decimais e trocando ponto por virgula 
+
+let number = 384.339988723
+ console.log(number.toFixed(2).replace('.', ','))
